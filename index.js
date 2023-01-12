a8782b2e45d101306b44ea181e86beab95efe6e8
Nohelia Osorio <nosorio1494@gmail.com>
2023-01-12 16:32:31 -0500

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = (2);
+let num2 = (31);
+var multiply = (num1 * num2);
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = (48);
+let num4 = (11);
+var mod = (num3 % num4);
+
+let max = (20);
+Math.max(20);
